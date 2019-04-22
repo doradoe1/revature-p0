17f03526df719e248b633d5999ad9853b9024b73
doradoe1 <doradoe@outlook.com>
2019-04-22 11:35:57 -0500

    Modified req one and three

--- azure-user.sh
+++ azure-user.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
 
-##Declare the variables needed. We will use these variables##
-##across the three functions.##
+##Declare the variables needed. We will use these variables.##
+##Across the three functions.##
 
 username=$1
-password=(-s $2)
-role=$4
-contributor=$(b24988ac-6180-42a0-ab88-20f7382dd24c)
-reader=$(acdd72a7-3385-48ef-bd42-f606fba81ae7)
 
 ##Step One: Login into Azure.##
 
-az login -u $username -p $password
+az login -u $username
 
 ##Step Two: Must have admin credentials to continue.##
 
-echo "Verifying for Administrator Credentials"
+echo "Verifying for Administrator Credentials. Please wait."
+
 check=$(az role assignment list  --include-classic-administrators \
 --query "[?id=='NA(classic admins)'].principalName" | grep -E $username)
 if [ -z $check ]; then
-  echo "You must have administrator credentials to use access this functionality" 1>&2
-  exit 1
+echo "You must have administrator credentials to use access this functionality" 1>&2
+exit 1
 fi
+
 echo "User Validated, please state one of the following functions:" \
-"Create user (createuser()), assing a role to an existing user (assignrole())," \
-" or delete a non-admin existing user (deleteuser())"
+"createuser username *no domain needed* to create a new user." \
+"assingrole create or delete username *no domain needed*" \
+"reader or contributor to assing or remove a role to an existing user." \
+"deleteuser username *no domain needed* to delete a non-admin existing user."
 
 ##Function One: Create User##
 ##Function will also check to see if the user we are intending to create already exits##
 
 createuser()
 {
+
+##Variables.##
+
 displayname=$1
 userprincipalname=$displayname@doradoe1outlook.onmicrosoft.com
 result=$(az ad user list --query [].userprincipalname | grep -E /$userprincipalname/)
+
 if [ -n 'result' ]; then
 echo "User being created. Please wait"
 az ad user create \
@@ -42,51 +46,54 @@ az ad user create \
 --user-principal-name $userprincipalname \
 --subscription 3bd3e6a7-a098-4e65-99d2-933b271e522a \
 --force-change-password-next-login true
-else
-echo "User already exist. Please make sure that you have entered the display name" \
-"correctly and try again" 1>&2
-exit 1
 fi
 
 echo 'Это сделано'
 exit 0
+
 }
 
 ##Function Two: Assing a Role.##
+
 assingrole()
 {
+
 ##Variables.##
-displayname=$1
+##Action value must be either create or delete.##
+##Role value must be either reader or contributor.##
+
+action=$1
+displayname=$2
 userprincipalname=$displayname@doradoe1outlook.onmicrosoft.com
-role=$2
-contributor=$(b24988ac-6180-42a0-ab88-20f7382dd24c)
-reader=$(acdd72a7-3385-48ef-bd42-f606fba81ae7)
+role=$3
 
 ##Checks if the user exists and if the role is already assigned or not.##
+
 if [ -z $userprincipalname ]; then
-echo "Invalid username. Please provide an existing displayname." 1>&2
+echo "Invalid username. Please provide an existing username." 1>&2
 exit 1
 elif [ -z $role ]; then
 echo "Invalid role. Please enter reader or contributor." 1>&2
 exit 1
 fi
 
-az role assignment create --assignee $userprincipalname --role $role
-
-az role assignment delete --assignee $userprincipalname --role $role
+az role assignment $action --assignee $userprincipalname --role $role
 
 }
 
 ##Function Three: Delete non-admin users.##
+
 deleteuser()
 {
+
 ##Variables.##
+
 displayname=$1
 userprincipalname=$displayname@doradoe1outlook.onmicrosoft.com
-role=$2
 
 ##Administration credentials checkpoint. Will not work procced if userprincipalname##
 ##belongs to an account administrator.##
+
 admincheck=$(az role assignment list \
 --include-classic-administrators \
 --query "[?id=='NA(classic admins)'].principalName" | grep -E $userprincipalname)
@@ -97,6 +104,7 @@ exit 1
 fi
 
 ##Verifying the existence of the user and deleting user from Azure Cloud##
+
 user=$(az ad user list --query [].userPrincipalName | grep -E $userprincipalname)
 if [ -z $user ]; then
 echo "Invalid user. Please enter an existing user with non-administrator credentials." 1>&2
@@ -105,4 +113,7 @@ else
 echo "User exists. Deleting User. Please wait."
 az ad user delete --upn-or-object-id $userprincipalname
 fi
+
 }
+
+$command $2 $3 $4
--- linux-setup.sh
+++ linux-setup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt-get install -y build-essential curl file git
+apt update
+apt-get install -y build-essential curl file git
 echo -ne '\n' | sh -c -y "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.profile
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-sudo apt install -y python3-distutils
+brew install git
 brew install node
-brew upgrade node
-brew install gcc
 brew install azure-cli
 
 echo 'You have a fully-functional DEATH-STAR'
 
+apt upgrade -y
+brew upgrade git
+brew update
+brew upgrade node
+brew upgrade az
+
+echo 'You have a fully-functional and up-to-date DEATH-STAR'
