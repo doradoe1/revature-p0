f60849056b829839b8c9a8088631b76b758d2c76
doradoe1 <doradoe@outlook.com>
2019-04-21 20:12:36 -0500

    Second Commit

--- azure-user.sh
+++ azure-user.sh
@@ -35,12 +35,17 @@ displayname=$1
 userprincipalname=$displayname@doradoe1outlook.onmicrosoft.com
 result=$(az ad user list --query [].userprincipalname | grep -E /$userprincipalname/)
 if [ -n 'result' ]; then
+echo "User being created. Please wait"
 az ad user create \
 --display-name $displayname \
 --password Revature2019 \
 --user-principal-name $userprincipalname \
 --subscription 3bd3e6a7-a098-4e65-99d2-933b271e522a \
 --force-change-password-next-login true
+else
+echo "User already exist. Please make sure that you have entered the display name" \
+"correctly and try again" 1>&2
+exit 1
 fi
 
 echo 'Это сделано'
@@ -48,11 +53,56 @@ exit 0
 }
 
 ##Function Two: Assing a Role.##
+assingrole()
+{
+##Variables.##
+displayname=$1
+userprincipalname=$displayname@doradoe1outlook.onmicrosoft.com
+role=$2
+contributor=$(b24988ac-6180-42a0-ab88-20f7382dd24c)
+reader=$(acdd72a7-3385-48ef-bd42-f606fba81ae7)
+
+##Checks if the user exists and if the role is already assigned or not.##
+if [ -z $userprincipalname ]; then
+echo "Invalid username. Please provide an existing displayname." 1>&2
+exit 1
+elif [ -z $role ]; then
+echo "Invalid role. Please enter reader or contributor." 1>&2
+exit 1
+fi
 
 az role assignment create --assignee $userprincipalname --role $role
 
 az role assignment delete --assignee $userprincipalname --role $role
 
-##Function Three: Delete non-admin users.
+}
 
+##Function Three: Delete non-admin users.##
+deleteuser()
+{
+##Variables.##
+displayname=$1
+userprincipalname=$displayname@doradoe1outlook.onmicrosoft.com
+role=$2
+
+##Administration credentials checkpoint. Will not work procced if userprincipalname##
+##belongs to an account administrator.##
+admincheck=$(az role assignment list \
+--include-classic-administrators \
+--query "[?id=='NA(classic admins)'].principalName" | grep -E $userprincipalname)
+if ! [ -z $admincheck ]; then
+echo "The user you are attempting to delete is an administrator." \
+"You do not have the permissions nessesary to delete administrators" 1>&2
+exit 1
+fi
+
+##Verifying the existence of the user and deleting user from Azure Cloud##
+user=$(az ad user list --query [].userPrincipalName | grep -E $userprincipalname)
+if [ -z $user ]; then
+echo "Invalid user. Please enter an existing user with non-administrator credentials." 1>&2
+exit 1
+else
+echo "User exists. Deleting User. Please wait."
 az ad user delete --upn-or-object-id $userprincipalname
+fi
+}
